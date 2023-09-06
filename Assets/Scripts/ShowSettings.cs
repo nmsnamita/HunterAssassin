187d112d3e0830e01f2cc6565c3d82146b4e4033
YashKeni <yashkeni007@gmail.com>
2023-09-06 20:02:31 +0530

    Updated Settings script

--- Assets/Scripts/ShowSettings.cs
+++ Assets/Scripts/ShowSettings.cs
@@ -14,10 +14,28 @@ public class ShowSettings : MonoBehaviour
     public void OpenSettings()
     {
         settingsPanel.SetActive(true);
+        PauseGame();
     }
 
     public void CloseSettings()
     {
+        StartCoroutine(UnPauseGame());
+    }
+
+    IEnumerator UnPauseGame()
+    {
+        ResumeGame();
+        yield return new WaitForSeconds(.1f);
         settingsPanel.SetActive(false);
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
     }
 }
