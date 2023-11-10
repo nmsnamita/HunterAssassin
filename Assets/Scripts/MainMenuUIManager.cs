00f53665ed8fa607b5f368727d1d85496724f005
YashKeni <yashkeni007@gmail.com>
2023-11-10 10:52:54 +0530

    Update MainMenuUIManager.cs

--- Assets/Scripts/MainMenuUIManager.cs
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -8,10 +8,19 @@ public class MainMenuUIManager : MonoBehaviour
 {
     [SerializeField] TMP_Text gemCountText;
 
+    [Header("Reward Buttons")]
+    [SerializeField] RewardsButton button1;
+    [SerializeField] RewardsButton button2;
+    [SerializeField] RewardsButton button3;
+
     public int gemCount;
 
     void Start()
     {
+        button1.Initialize(this);
+        button2.Initialize(this);
+        button3.Initialize(this);
+
         gemCount = PlayerDataManager.LoadGemCount();
         gemCountText.text = gemCount.ToString();
     }
@@ -21,9 +30,9 @@ public class MainMenuUIManager : MonoBehaviour
         gemCountText.text = gemCount.ToString();
     }
 
-    public void IncreaseGems()
+    public void IncreaseGems(RewardsButton button)
     {
-        gemCount += FindObjectOfType<ShopItemButton>().gemValue;
+        gemCount += button.gemValue;
         PlayerDataManager.SaveGemCount(gemCount);
     }
 
