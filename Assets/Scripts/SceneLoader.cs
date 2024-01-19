af510a760ed584210938b2a4f8662a928405edaf
Shubham Tirlotkar <shubham.tirlotkar@nmsgames.in>
2024-01-19 17:24:52 +0530

    levels total is now 120

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
